5910b4d41308e1e83c9d6d165b1132592379754f
dheerajy7695 <dheerajy906@gmail.com>
2020-06-08 14:25:12 +0530

    changes in db connection

--- api/config/dbSource.js
+++ api/config/dbSource.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dbUrl: 'mongodb://localhost:27017/DummayApplication'
+    dbUrl: 'mongodb+srv://testAdmin:Node_pwd65@dheeraj-mongodb-sofjk.mongodb.net/test?authSource=admin'
 }
