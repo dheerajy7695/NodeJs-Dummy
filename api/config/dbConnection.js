07b84ba542de3abd2c1204f7a793a5bd4e6f6faa
dheerajy7695 <dheerajy7695@gmail.com>
2022-10-09 12:39:26 +0530

    Added generate toke functionality

--- api/config/dbConnection.js
+++ api/config/dbConnection.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 const dbUrl = { dbUrl: "mongodb://localhost:27017/localdb1" };
 
+mongoose.set('useCreateIndex', true);
 module.exports.dbConnection = () => {
-    mongoose.connect(dbUrl.dbUrl, { useNewUrlParser: true })
+    mongoose.connect(dbUrl.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log('Database connected successfully');
             return 'Connection established successfully';
--- api/modules/users/routes/user.routes.js
+++ api/modules/users/routes/user.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user.controller');
+const isUserAuthenticated = require('../../../core/middleware/authMiddleware');
 
 router.get('/getUser', userController.getUsers);
 router.post('/create-user', userController.createUser);
-router.post('/loginUser', userController.loginUser);
+router.post('/loginUser', isUserAuthenticated, userController.loginUser);
 router.get('/getById/:userId', userController.getUserById);
 router.get('/getByUsername/:username', userController.getUserByUsername);
 router.delete('/delete-user/:userId', userController.deleteUser);
